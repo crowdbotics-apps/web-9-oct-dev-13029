e994d742d5121c5572a7ec270f27851b8a45ade3
Crowdbotics <team@crowdbotics.com>
2020-10-09 14:08:10 +0000

    Update models

--- users/migrations/0002_auto_20201009_1404.py
+++ users/migrations/0002_auto_20201009_1404.py
@@ -6,23 +6,31 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0001_initial'),
+        ("users", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='UUser',
+            name="UUser",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('username', models.CharField(max_length=100)),
-                ('email', models.EmailField(max_length=290)),
-                ('password', models.CharField(max_length=100)),
-                ('hgfhgfhg', models.SmallIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("username", models.CharField(max_length=100)),
+                ("email", models.EmailField(max_length=290)),
+                ("password", models.CharField(max_length=100)),
+                ("hgfhgfhg", models.SmallIntegerField()),
             ],
         ),
         migrations.AlterField(
-            model_name='user',
-            name='name',
+            model_name="user",
+            name="name",
             field=models.CharField(blank=True, max_length=255, null=True),
         ),
     ]
